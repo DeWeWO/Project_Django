a52bc636d2ae34992ee4c3d3d25a585f0a9e23aa
DeWeW <dewel000per@gmail.com>
2025-07-12 17:50:01 +0500

    django project

--- furni/views.py
+++ furni/views.py
@@ -1,7 +1,7 @@
 import requests
 from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product, ProductImage, Category
+from .models import Product, ProductImage, Category, Comment
 from .forms import ProductForm, ProductUpdateForm
 from django.db.models import Avg, Max, Min, Sum, F, ExpressionWrapper, DecimalField, Value, Case, When, Q
 from django.db.models.functions import Round, Coalesce
@@ -30,24 +30,6 @@ def add_product(request):
     return render(request, "shop/add_product.html", {"form": form})
     
 
-# def product_list(request, category_slug = None):
-#     products = Product.objects.all()
-#     if category_slug:
-#         category = get_object_or_404(Category, slug=category_slug)
-#         products = products.filter(category=category)
-#     search = request.GET.get("search")
-#     if search:
-#         products = products.filter(Q(title__icontains=search) | Q(description__icontains=search))
-#     products.annotate(
-#         sum_discount=Sum("discount__percent"),
-#         new_price = ExpressionWrapper(
-#             Round((100 - F("sum_discount")) * F("price") / 100, 2),
-#         output_field=DecimalField(decimal_places=2, max_digits=12))).order_by("-id")
-#     paginator = Paginator(products, 3)
-#     page_number = request.GET.get("page")
-#     products = paginator.get_page(page_number)
-#     return render(request, "shop/products.html", {"products": products})
-
 def product_list(request, category_slug=None):
     products = Product.objects.select_related("category").all()
     if category_slug:
@@ -72,7 +54,8 @@ def product_list(request, category_slug=None):
 @login_required(login_url="/auth/login/")
 def product_detail(request, product_slug):
     product = Product.objects.filter(slug=product_slug).select_related("author").prefetch_related("images").first()
-    return render(request, "shop/product_detail.html", {"product": product})
+    comments = Comment.objects.filter(product=product)
+    return render(request, "shop/product_detail.html", {"product": product, "comments": comments})
 
 
 @login_required(login_url="/auth/login/")
