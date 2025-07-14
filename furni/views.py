3ca1f7820ec687d7c4d47b92d3e4a2f1d3896ab6
DeWeW <dewel000per@gmail.com>
2025-07-14 13:41:20 +0500

    django project

--- furni/views.py
+++ furni/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, ProductImage, Category, Comment
 from .forms import ProductForm, ProductUpdateForm
-from django.db.models import Avg, Max, Min, Sum, F, ExpressionWrapper, DecimalField, Value, Case, When, Q, BooleanField
+from django.db.models import Avg, Max, Min, Sum, F, ExpressionWrapper, DecimalField, Value, Case, When, Q, BooleanField, Count
 from django.db.models.functions import Round, Coalesce
 from django.core.paginator import Paginator
 from environs import Env
@@ -56,7 +56,7 @@ def product_detail(request, product_slug):
     product = Product.objects.filter(slug=product_slug).select_related("author").prefetch_related("images").first()
     if request.method == "POST":
         Comment.objects.create(author=request.user, product=product, message=request.POST["comment"])
-    comments = Comment.objects.filter(product=product).annotate(like=Value(True, output_field=BooleanField()))
+    comments = Comment.objects.filter(product=product).annotate(likes_count=Count("likes"))
     return render(request, "shop/product_detail.html", {"product": product, "comments": comments})
 
 
